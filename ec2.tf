a79a15bf4ba5220778ac25467b51afe3e38e37b9
swagat030 <swagatkumarmishra888@gmail.com>
2022-06-27 21:33:23 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,17 +5,20 @@ resource "aws_instance" "myec2" {
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   key_name               = "ubuntu-keypair"
   tags = {
-    name = "minikube_instance"
+    Name = "minikube_instance"
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update && apt -y install docker.io",
-      "curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl &&   chmod +x ./kubectl && sudo mv ./kubectl /usr/local/bin/kubectl",
-      "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo apt update -y",
+      "sudo apt install docker.io -y",
+      "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+      "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
       "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-      "sudo chmod 755 /usr/local/bin/minikube",
-      "sudo apt install conntrack"
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+      "sudo chmod +x /home/ubuntu/kubectl",
+      "sudo cp kubectl /usr/local/bin/kubectl",
+      "sudo usermod -aG docker ubuntu"
     ]
     connection {
       type        = "ssh"
