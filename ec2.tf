08e836e07d301d069ed4150b0f2bd4f514f6bbf8
swagat030 <swagatkumarmishra888@gmail.com>
2022-07-01 02:51:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -28,8 +28,7 @@ resource "aws_instance" "myec2" {
       "minikube version",
       "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
       "chmod +x kubectl",
-      "sudo mv kubectl /usr/local/bin/",
-      "minikube start"
+      "sudo mv kubectl /usr/local/bin/"
     ]
     connection {
       type        = "ssh"
