81a6b3d5ec63095338b8b5e6e4338aa020cd8e8c
swagat030 <swagatkumarmishra888@gmail.com>
2022-07-01 01:51:05 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "myec2" {
       "sudo apt update",
       "sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      "echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
       "sudo apt-get update",
       "sudo apt-get install docker-ce docker-ce-cli containerd.io -y",
       "sudo usermod -aG docker $USER",
