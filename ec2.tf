fbc68e2ec05b54343e8ec74f9b1b8c34b1abaed9
swagat030 <swagatkumarmishra888@gmail.com>
2022-07-01 01:43:16 +0530

    file updated

--- ec2.tf
+++ ec2.tf
@@ -10,14 +10,27 @@ resource "aws_instance" "myec2" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update -y",
-      "sudo apt install docker.io -y",
-      "sudo wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
-      "sudo chmod +x /home/ubuntu/minikube-linux-amd64",
+      "sudo apt update",
+      "sudo apt install curl wget apt-transport-https -y",
+      "sudo apt update",
+      "sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      "echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io -y",
+      "sudo usermod -aG docker $USER",
+      "newgrp docker",
+      "docker version",
+      "docker run hello-world",
+      "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
       "sudo cp minikube-linux-amd64 /usr/local/bin/minikube",
-      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
-      "sudo chmod +x /home/ubuntu/kubectl",
-      "sudo cp kubectl /usr/local/bin/kubectl"
+      "sudo chmod +x /usr/local/bin/minikube",
+      "minikube version",
+      "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl"
+      "chmod +x kubectl",
+      "sudo mv kubectl /usr/local/bin/",
+      "kubectl version",
+      "minikube start --driver=docker"
     ]
     connection {
       type        = "ssh"
